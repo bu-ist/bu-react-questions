621df03219fce74baedf9131cbc0dbf4d112359a
Carlos E Silva <cedas@bu.edu>
2018-11-09 10:24:49 -0500

    Start development of TrueFalse component

---
+++ src/lib/questions/TrueFalse.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class TrueFalse extends React.Component {
+  render() {
+    return (
+      <ul>
+        <li>True</li>
+        <li>False</li>
+      </ul>
+    );
+  }
+}
+
+export default TrueFalse;
