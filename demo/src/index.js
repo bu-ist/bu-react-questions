44e2cc20215ea012221d748fb80abc78e824cd0e
Carlos E Silva <cedas@bu.edu>
2018-12-10 15:53:44 -0500

    Move from cra to nwb

---
+++ demo/src/index.js
@@ -0,0 +1,121 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+import { Question } from "../../src";
+
+const trueFalseQuestionData = {
+  type: "true-false",
+  header: "Is the following statement true or false",
+  body: "The square root of 4 is 2",
+  answers: [
+    {
+      answer: "True",
+      correct: true
+    },
+    {
+      answer: "False",
+      correct: false
+    }
+  ],
+  correctFeedback: "Wow, you are like a doctor or a rocket scientist!",
+  incorrectFeedback: "Are you a kindergarden dropout?"
+};
+
+const multipleAnswerQuestionData = {
+  type: "multiple-answer",
+  header: "Please select all correct answers",
+  body: "How many toes can a two toed sloth have?",
+  answers: [
+    {
+      answer: "6",
+      feedback: "You are correct! They some of them can have six.",
+      correct: true
+    },
+    {
+      answer: "0",
+      feedback: "Nope, definitely more than that",
+      correct: false
+    },
+    {
+      answer: "10",
+      feedback: "Nope, that is too many",
+      correct: false
+    },
+    {
+      answer: "8",
+      feedback: "You are correct! They some of them can have eight.",
+      correct: true
+    },
+    {
+      answer: "100",
+      feedback: "Really?",
+      correct: false
+    }
+  ],
+  correctFeedback: "You picked all of the correct answers!",
+  incorrectFeedback:
+    "Sorry, see the feedback below your answers for why it was wrong."
+};
+
+const multipleChoiceQuestionData = {
+  type: "multiple-choice",
+  header: "Please select the correct answer",
+  body: "How many toes does a two toed sloth have?",
+  answers: [
+    {
+      answer: "None",
+      feedback: "Nope, definitely more than that",
+      correct: false
+    },
+    {
+      answer: "Ten",
+      feedback: "Nope, that is too many",
+      correct: false
+    },
+    {
+      answer: "Two",
+      feedback: "Nope, a little more than that.",
+      correct: false
+    },
+    {
+      answer: "Either six or eight",
+      feedback:
+        'You are correct! The name "two-toed sloth" erroneously describe the number of toes.',
+      correct: true
+    },
+    {
+      answer: "All of the above",
+      feedback: "Really?",
+      correct: false
+    }
+  ],
+  correctFeedback: "You picked the correct answer!",
+  incorrectFeedback:
+    "You picked an incorrect answer, see the feedback below your answer for why it was wrong."
+};
+
+const calculatedNumericQuestionData = {
+  type: "calculated-numeric",
+  header: "Submit the correct answer:",
+  body: "What is the result of 1/3",
+  answer: "0.33",
+  answerRange: "0.001",
+  decimalPlaces: "3",
+  feedback: "1 divided by 3 is 0.333333333"
+};
+
+class Demo extends Component {
+  render() {
+    return (
+      <div>
+        <h1>react-questions Demo</h1>
+        <Question questionData={trueFalseQuestionData} />
+        <Question questionData={multipleChoiceQuestionData} />
+        <Question questionData={multipleAnswerQuestionData} />
+        <Question questionData={calculatedNumericQuestionData} />
+      </div>
+    );
+  }
+}
+
+render(<Demo />, document.querySelector("#demo"));
